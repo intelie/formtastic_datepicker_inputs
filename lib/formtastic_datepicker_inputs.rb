a53cdc5de85e177b2436761694b6a0f55328dda2
Sergio Junior <sergior.jr@gmail.com>
2011-06-08 17:26:05 -0300

    date component fix

--- lib/formtastic_datepicker_inputs.rb
+++ lib/formtastic_datepicker_inputs.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # FormtasticDatepickerInputs
 #To get this solution working in Rails 3, replace:
 #
@@ -18,28 +19,40 @@ module Formtastic
 
     def date_picker_input(method, options = {})
       format = options[:format] || DATE_FORMATS[:default] || '%d %b %Y'
-      clss = []
-      inp_opts = options.delete(:input_html) || {}
-      clss << inp_opts.delete(:class).to_s
-      clss << options.delete(:class).to_s
-      options[:input_html] = {:class => "ui-date-picker " + clss.join(' ')}.merge inp_opts
-      options[:value] = options[:value] || object.send(method).try(:strftime, format)
-      basic_input_helper(:text_field, :string, method, options)
+      
+      opts = date_picker_options(format, options, object.send(method))
+      string_input(method, opts)
+    end
+
+    # Generate html input options for the datepicker_input
+    #
+    def date_picker_options(format, opts, value = nil)
+      
+      input_opts = {:class => 'ui-date-picker',:value => value.try(:strftime, format)}
+      input_opts[:class] = (input_opts[:class].concat(' ')).concat(opts.delete(:class)) if opts[:class]
+      input_opts = input_opts.merge(opts.delete(:input_html)) if opts[:input_html]
+      
+      opts.merge({:input_html => input_opts})
     end
   end
   
   module DateTimePicker
     protected
 
-    def datetime_picker_input(method, options = {}, value = nil)
+    def datetime_picker_input(method, options = {})
       format = options[:format] || DATE_FORMATS[:default] || '%d %b %Y %H:%M'
-      clss = []
-      inp_opts = options.delete(:input_html) || {}
-      clss << inp_opts.delete(:class).to_s
-      clss << options.delete(:class).to_s
-      options[:input_html] = {:class => "ui-date-picker " + clss.join(' ')}.merge inp_opts
-      options[:value] = options[:value] || object.send(method).try(:strftime, format)
-      basic_input_helper(:text_field, :string, method, options)
+      string_input(method, datetime_picker_options(format, options, object.send(method)))
+    end
+
+    # Generate html input options for the datepicker_input
+    #
+    def datetime_picker_options(format, opts, value = nil)
+      input_opts = {:class => 'ui-datetime-picker', :value => value.try(:strftime, format)}
+      input_opts[:class] = (input_opts[:class].concat(' ')).concat(opts.delete(:class)) if opts[:class]
+      input_opts = input_opts.merge(opts.delete(:input_html)) if opts[:input_html]
+      
+      {:wrapper_html => {:class => 'datetime'}, 
+        :input_html => input_opts }.merge(opts)
     end
   end
 end
